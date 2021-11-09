6b945211605ba2a0a9e3fb3a0604df5d245bf478
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-11-08 21:48:35 -0500

    first commit

--- main.js
+++
@@ -1,8 +0,0 @@
-import { Rater } from "./rater.js";
-
-document.addEventListener("DOMContentLoaded", function () {
-  const raters = document.querySelectorAll("[role=rater]");
-  raters.forEach((rater) => {
-    new Rater(rater);
-  });
-});
